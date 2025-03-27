e05d4874cc16fb3f223b3632bbb6328996f969c7
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-03-27 11:28:27 +0530

    com 3

--- frontend/src/components/others/Header.jsx
+++ frontend/src/components/others/Header.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
     <div className="flex items-end justify-between">
       <h1 className="text-2xl font-medium">
         Hello <br />
-        <span className="text-3xl font-semibold"> sanajy👋</span>
+        <span className="text-3xl font-semibold"> User👋</span>
       </h1>
       <button
         onClick={logOutUser}
